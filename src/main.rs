101aa58339220e85f88e0cf941054bafaf00298a
0x00whitecode <x10tion007@gmail.com>
2025-04-21 16:54:42 +0100

    aaded full comment

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,17 @@
-#[allow(unused_imports)]
 use std::net::TcpListener;
+use std::io::Write; // Needed for writing to the stream
 
 fn main() {
-    // You can use print statements as follows for debugging, they'll be visible when running tests.
-    //println!("Logs from your program will appear here!");
+    let listener = TcpListener::bind("127.0.0.1:4221").unwrap();
 
-    // Uncomment this block to pass the first stage
-    //
-     let listener = TcpListener::bind("127.0.0.1:4221").unwrap();
-    //
     for stream in listener.incoming() {
         match stream {
-            Ok(_stream) => {
+            Ok(mut stream) => {
                 println!("accepted new connection");
+
+                // Write the minimal HTTP response
+                let response = "HTTP/1.1 200 OK\r\n\r\n";
+                stream.write_all(response.as_bytes()).unwrap();
             }
             Err(e) => {
                 println!("error: {}", e);
